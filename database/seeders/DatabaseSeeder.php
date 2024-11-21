fb6f7ae6c16ec09713e9fe740f0f6fcef8c0e92d
azharazm1 <azharazmi.ke@gmail.com>
2024-11-21 17:03:44 +0800

    1st-commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        /** Memanggil model lalu menjalankan fungsi factory */
+        Article::factory(8)->create();
+        
+        // User::factory(10)->create();
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
